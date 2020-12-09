0edb03bad1b5aeb88b81c2dc128e8c1a71018ef9
JulianReid541 <reidjulian@outlook.com>
2020-12-09 15:24:52 -0800

    spinner 100% complete

--- spinnerbutton/src/App.js
+++ spinnerbutton/src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react';
+import styled, { keyframes, css } from 'styled-components';
+import { rgba, darken, lighten } from 'polished';
 
-function App() {
+const colorPrimary = "#6e45bc";
+
+const spin = keyframes`
+  100% { transform: rotate(360deg); }
+`;
+
+const Spinner = styled.span`
+  display: inline-block;
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  border-width: 5px;
+  border-style: solid;
+  border-color: ${rgba("white", 0.1)};
+  border-top-color: white;
+  animation: ${spin} 1s infinite linear;
+`;
+
+const spinnerOpenStyles = css`
+  visibility: visible; 
+  opacity: 1;
+`;
+
+const SpinnerWrapper = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: ${darken(0.1, colorPrimary)};
+  visibility: hidden;
+  opacity: 0;
+  transition-property: visibility, opacity;
+  transition-duration: 0.3s;
+  ${({ isVisible }) => isVisible && spinnerOpenStyles};
+`;
+
+const fadeIn = keyframes`
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+`;
+
+const FadeIn = styled.span`
+  display: flex;
+  align-items: center;
+  line-height: 1;
+  animation: ${fadeIn} 0.25s;
+`;
+
+const Button = styled.button`
+  position: relative;
+  overflow: hidden;
+  min-width: 180px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 0;
+  padding: 0;
+  background: ${colorPrimary};
+  height: 64px;
+  border-radius: 0.5rem;
+  letter-spacing: 1px;
+  font-weight: 600;
+  font-size: 1.1rem;
+  color: white;
+  outline: none;
+  cursor: pointer;
+  transition: background 0.25s;
+`;
+
+const SpinnerButton = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isComplete, setIsComplete] = useState(false);
+
+  const handleClick = useCallback(() => {
+    setIsLoading(true);
+    setTimeout(() =>{
+      setIsLoading(false);
+      setIsComplete(true);
+      setTimeout(() => {
+        setIsComplete(false);
+      }, 3000);
+    }, 2500)
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Button 
+      onClick={handleClick}
+      disabled={isLoading || isComplete}
+    >
+      {!isComplete && "Sign Up"}
+      {isComplete && "All Done"}
+      <SpinnerWrapper isVisible={isLoading}>
+        <Spinner />
+      </SpinnerWrapper>
+    </Button>
   );
-}
+};
 
-export default App;
+export default SpinnerButton;
